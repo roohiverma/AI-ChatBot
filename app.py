c7fccdedd2010e44bc8987b0d9404c6c805c3513
Roohi Verma <roohivermapb@gmail.com>
2020-03-16 22:30:10 -0400

    adding basic app

---
+++ app.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__) 
+ 
+@app.route('/') 
+def hello_world(): 
+	return render_template('index.html')
+
+# main driver function 
+if __name__ == '__main__': 
+
+
+	app.run(debug=True) 
